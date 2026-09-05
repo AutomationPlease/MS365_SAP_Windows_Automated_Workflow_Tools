c87d6f07a80c8a274f958a6b3ed9a6ac6532d1de
Lawrence Willcox <lawrencewillcox@gmail.com>
2026-09-04 23:02:04 -0500

    Delete Windows_OS_Tools directory

--- Windows_OS_Tools/Keep_Alive/start_automation.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-:: obviously adjust to your computer, demonstration below to show structure, added delay so they don't fight to start.
-:: add additional helper scripts always after starting keep_alive first.
-
-start "" ".venv\Scripts\pythonw.exe" "keep_alive.pyw"
-timeout /t 2 >nul
-start "" ".venv\Scripts\pythonw.exe" "smart_mouse_clicker.pyw"
